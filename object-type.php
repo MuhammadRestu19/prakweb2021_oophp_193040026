a360ccc993327422da3940e303053d6daf24e0fb
restu1909 <58680735+restu1909@users.noreply.github.com>
2021-09-16 13:08:54 +0700

    menyelesaikan latihan di video ke 4 membuat object type

---
+++ object-type.php
@@ -0,0 +1,50 @@
+<?php
+
+use Produk as GlobalProduk;
+
+class Produk {
+    public  $judul ,
+            $penulis ,
+            $penerbit ,
+            $harga ;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+}
+
+
+
+class CetakInfoProduk{
+    public function cetak(Produk $produk){
+        $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+
+
+
+$produk1 = new Produk("naruto", "masashi kishimoto", "shonen jump", 30000);
+$produk2 = new Produk("uncharted", "neil druckmann", "sony computer", 250000);
+
+echo "komik : " . $produk1->getLabel();
+echo "<br>";
+echo "game : " . $produk2->getLabel();
+echo "<br>";
+
+$infoproduk1 = new CetakInfoProduk();
+echo $infoproduk1->cetak($produk1)
+
+?>
